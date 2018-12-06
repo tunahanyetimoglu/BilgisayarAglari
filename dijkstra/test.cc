ee2876808b2447df735a1a27f5d824ab581e8829
tunahanyetimoglu <tunahan.yetimoglu@bil.omu.edu.tr>
2018-12-06 15:32:22 +0300

    Update test.cc

--- dijkstra/test.cc
+++ dijkstra/test.cc
@@ -13,73 +13,103 @@
 
 using namespace omnetpp;
 
-class testComputer : public cSimpleModule{
+class yedekComputer : public cSimpleModule{
 protected:
     virtual void initialize() override;
     virtual void handleMessage(cMessage *msg) override;
 };
 
-Define_Module(testComputer);
+Define_Module(yedekComputer);
 
-void testComputer :: initialize(){
 
-    std::vector<std::string> nedTypes;
-    nedTypes.push_back("testComputer");
-    cTopology *topo = new cTopology("topo");
-    topo->extractByNedTypeName(nedTypes);
-    for(int i=0;i<4;i++){
-        cTopology::Node *node = topo->getNode(i);
-         if(i == 0) {
-            node->setWeight(5);
-         }else if(i == 1){
-            node->setWeight(1);
-         }else if(i == 2){
-            node->setWeight(30);
-         }else if(i == 3){
-            node->setWeight(1);
-         }else{
-            node-> setWeight(10);
-         }
-    }
-    EV << "Node : " << getName() << "       -   Weight : " << sourceNode->getWeight() << "\n";
-    if (strcmp("computer", getName()) == 0){
-        
+
+void yedekComputer :: initialize(){
+        //an STL string vector
+        std::vector<std::string> nedTypes;
+        nedTypes.push_back("yedekComputer");
+        cTopology *topo = new cTopology("topo");
+        // Extracting the topology from a network.
+        //Extracts model topology by the fully qualified NED type name of the modules.
+        //All modules whose getNedTypeName() is listed in the given string vector will get included.
+        topo->extractByNedTypeName(nedTypes);
+        for(int i=0;i<4;i++){
+                                  cTopology::Node *node = topo->getNode(i);
+                                  if(i == 0) {
+                                      node->setWeight(5);
+                                  }else if(i == 1){
+                                      node->setWeight(1);
+                                  }else if(i == 2){
+                                      node->setWeight(30);
+                                  }else if(i == 3){
+                                      node->setWeight(1);
+                                  }else{
+                                      node-> setWeight(10);
+                                  }
+
+                             }
         cTopology::Node *sourceNode = topo->getNodeFor(this);
-        sourceNode->disable();
-           
-        cTopology::Node *destNode = topo->getNode(atoi(DEST_NODE));
-        sourceNode->enable();
-        topo->calculateWeightedSingleShortestPathsTo(destNode);
-            
-        cGate *c = sourceNode->getPath(0)->getLocalGate();
-                   char msgname[20];
-                    sprintf(msgname, "Node : -%d", getIndex());
-                    cMessage *msg = new cMessage(msgname);
-                   send(msg,c);
+        EV << "Node : " << getName() << "       -   Weight : " << sourceNode->getWeight() << "\n";
+        if (strcmp("computer", getName()) == 0) {
+            //Returns the graph node which corresponds to the given module in the network.
+            //If no graph node corresponds to the module, the method returns NULL.
+            //This method assumes that the topology corresponds to the network, that is, it was probably created with one of the extract...() functions.
+            cTopology::Node *sourceNode = topo->getNodeFor(this);
+
+            //Returns pointer to the ith node in the graph.
+            //Node's methods can be used to further examine the node's connectivity, etc.
+            cTopology::Node *destNode = topo->getNode(atoi(DEST_NODE));
+
+            //Apply the Dijkstra algorithm to find all shortest paths to the given graph node.
+            //The paths found can be extracted via Node's methods.
+            //topo->calculateUnweightedSingleShortestPathsTo(destNode);
+
+
+            //Apply the Dijkstra algorithm to find all shortest paths to the given graph node.
+            //The paths found can be extracted via Node's methods.     !--- Uses weights in nodes and links. ---!
+            topo->calculateWeightedSingleShortestPathsTo(destNode);
+
+            cGate *c = sourceNode->getPath(0)->getLocalGate();      // getLocalGate:  returns  the gates at the local  end of this connection
+            char msgname[20];
+            sprintf(msgname, "Source Node : %s", getName());
+            cMessage *msg = new cMessage(msgname);
+            send(msg,c);
         }
-}
-void testComputer::handleMessage(cMessage *msg)
+    }
+void yedekComputer::handleMessage(cMessage *msg)
 {
         std::vector<std::string> nedTypes;
-        nedTypes.push_back("testComputer");
+        nedTypes.push_back("yedekComputer");
 
         //Create cTopology Object
         cTopology *topo = new cTopology("topo");
 
+        // Extracting the topology from a network.
+        //Extracts model topology by the fully qualified NED type name of the modules.
+        //All modules whose getNedTypeName() is listed in the given string vector will get included.
         topo->extractByNedTypeName(nedTypes);
-        
+
+        //Returns the graph node which corresponds to the given module in the network.
+        //If no graph node corresponds to the module, the method returns NULL.
+        //This method assumes that the topology corresponds to the network, that is, it was probably created with one of the extract...() functions.
         cTopology::Node *sourceNode = topo->getNodeFor(this);
-        sourceNode->disable();
 
+
+        //Returns pointer to the ith node in the graph.
+        //Node's methods can be used to further examine the node's connectivity, etc.
         cTopology::Node *destNode = topo->getNode(atoi(DEST_NODE));
-        sourceNode->enable();
 
+        //Apply the Dijkstra algorithm to find all shortest paths to the given graph node.
+        //The paths found can be extracted via Node's methods.
+        //topo->calculateUnweightedSingleShortestPathsTo(destNode);
+
+        //Apply the Dijkstra algorithm to find all shortest paths to the given graph node.
+        //The paths found can be extracted via Node's methods.     !--- Uses weights in nodes and links. ---!
         topo->calculateWeightedSingleShortestPathsTo(destNode);
-
         if(sourceNode->getNumPaths() != 0){
             cGate *c = sourceNode->getPath(0)->getLocalGate();
-            simtime_t eed = simTime() - msg->getCreationTime();
-
+            char msgname[20];
+            sprintf(msgname, "Node : %s", getName());
+            cMessage *msg = new cMessage(msgname);
             send(msg,c);
         }
 }
